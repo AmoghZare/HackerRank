2148e93af69290d3dff16ca56b605a22e74a9f6b
AmoghZare <49691723+AmoghZare@users.noreply.github.com>
2019-08-12 14:08:52 +0530

    Update SherlockandtheValidString.py

--- SherlockandtheValidString.py
+++ SherlockandtheValidString.py
@@ -1,1 +1,62 @@
+#!/bin/python3
+#Problem: Sherlock and the Valid String
+#18/20 Test Cases Passed
+#Difficulty: Medium
+
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the isValid function below.
+def isValid(s):
+    frequency = []
+    n = len(s)
+    temp = s
+    for i in range(n):
+        if i < len(temp):
+            count = checkCount(temp[i],temp)
+            frequency.append(count)
+            temp = "".join(temp.rsplit(temp[i],count-1))
+            temp = temp.replace('','')
+    return (checkValidity(frequency))
+
+def checkValidity(frequency):
+    n = len(frequency)
+    differentFlag = 0
+    differentCount = 0
+    maximumFrequency = max(frequency,key=frequency.count)
+    for index in range(n):
+        if maximumFrequency == frequency[index]:
+            differentFlag = 0
+        elif maximumFrequency != frequency[index]:
+            differentFlag = 1
+            differentCount +=1
+                
+    if differentCount>1:
+        return 'NO'
+    else:
+        return 'YES'
+        
+def checkCount(char,string):
+    n = len(string)
+    count = 0
+    for index in range(n):
+        if char == string[index]:
+            count+=1
+    return count
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = isValid(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
 
